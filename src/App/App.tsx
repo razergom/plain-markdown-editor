b36dea0b37ec27b3537680f275869a2e1e5a3d6e
Andrey Petrov <thenolldd@gmail.com>
2021-04-09 20:01:11 +0300

    feat markdown preview

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
+import marked from 'marked'
 import { CustomEditor } from '../Components/CustomEditor';
 import styles from './styles.module.scss'
 
 export const App = () => {
-    const [editorContent, setEditorContent] = useState('Epic Markdown Editor Content')
+    const [editorContent, setEditorContent] = useState('')
+
+    const generateMarkdownPreview = (markdownText: string) => ({ __html: marked(markdownText)})
 
     return (
         <div className={styles.app}>
             <div className={styles.editorWrapper}>
-                <h2>Epic Markdown Editor</h2>
+                <h2>simple markdown editor</h2>
                 <CustomEditor
                     value={editorContent}
                     onChange={setEditorContent}
@@ -22,6 +25,10 @@ export const App = () => {
                         }
                     }
                 />
+            </div>
+            <div className={styles.previewWrapper}>
+                <h2>simple markdown preview</h2>
+                <div className={styles.preview} dangerouslySetInnerHTML={generateMarkdownPreview(editorContent)} />
             </div>
         </div>
     )
